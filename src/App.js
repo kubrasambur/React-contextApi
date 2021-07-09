6969497c3ef7c7136b0134de3ee503ba5d630543
kubrasambur <kubrasambur123@gmail.com>
2021-07-09 11:54:06 +0300

    09.07.21

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import React from "react";
 import BookList from "./components/BookList";
 import BookContextProvider from "./contexts/BookContext";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <BookContextProvider>
-          <BookList />
-        </BookContextProvider>
+        <ThemeContextProvider>
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
+        </ThemeContextProvider>
       </div>
     );
   }
 }
 
 export default App;
+
+
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,24 +2,26 @@ import React from "react";
 import Book from "./Book";
 import "./BookList.css";
 import { BookContext } from "../contexts/BookContext";
-
-// 1 - ) Default value ---> this.context
-// - Context nesnesini oluşturduk
-// başlangıç değerini verdik ve export ettik
-// ilgili componenta import ettik
-// context type yardımıyla contexti ilgili componente atadık
-// this.context yardımıyla ilgili veriye ulaştık.
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class BookList extends React.Component {
   render() {
     return (
-      <BookContext.Consumer>
-          {value => {
-              return(
-                  <section className="page-section" id="portfolio">
+      <ThemeContext.Consumer>
+        {(contextTheme) => (
+          <BookContext.Consumer>
+             {(ContextBook) => {
+                 const {books} =ContextBook
+              const {isDarkTheme, dark,light} = contextTheme;
+              const theme = isDarkTheme ? dark : light;
+
+              return (
+                <section className="page-section" style={{background: theme.bg, color:theme.txt}} id="portfolio">
                   <div className="container">
                     <div className="text-center">
-                      <h2 className="section-heading text-uppercase">BookFolio</h2>
+                      <h2 className="section-heading text-uppercase">
+                        BookFolio
+                      </h2>
                       <h3 className="section-subheading text-muted">
                         Lorem ipsum dolor sit amet consectetur.
                       </h3>
@@ -32,16 +34,17 @@ export default class BookList extends React.Component {
                       </button>
                     </div>
                     <div className="row">
-                      {value.books.map((book, i) => {
+                      {books.map((book, i) => {
                         return <Book book={book} key={i} />;
                       })}
                     </div>
                   </div>
                 </section>
-              )
-          }}
-      </BookContext.Consumer>
-
-  );
+              );
+            }}
+          </BookContext.Consumer>
+        )}
+      </ThemeContext.Consumer>
+    );
   }
 }
